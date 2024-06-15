c31132ccf6861222697720f09e737a9eb0d4b638
msibrahimhussain <141616747+msibrahimhussain@users.noreply.github.com>
2024-06-15 06:11:12 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -31,12 +31,12 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Frontend: HTML, CSS, Bootstrap, JavaScript, React.js",
+  "Backend: Python,Express, Node.js",
+  "Databases: SQLite",
+  "Networking",
+  "Power Electronics",
+  "Embedded Systems",
 ];
 
 /**
